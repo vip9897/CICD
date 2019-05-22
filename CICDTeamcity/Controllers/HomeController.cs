e2470301d632d309981d205e3437121524ce88be
Vipin <Vipin@DESKTOP-PCC8FJC>
2019-05-23 01:21:38 +0530

    asaaaaaaaaaaaa

--- CICDTeamcity/Controllers/HomeController.cs
+++ CICDTeamcity/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace CICDTeamcity.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index();
         {
             return View();
         }
